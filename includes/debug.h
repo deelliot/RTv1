cd50429364bed4623bd7c3f248425d3baee09bdf
Teemu Hakala <thakala@c1r5p10.hive.fi>
2022-10-20 21:41:00 +0300

    unravel_sphere functionality, to be done verbose switch usage, logically untested (compiles)

--- includes/debug.h
+++ includes/debug.h
@@ -6,13 +6,41 @@
 /*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 08:19:24 by thakala           #+#    #+#             */
-/*   Updated: 2022/10/19 08:49:02 by thakala          ###   ########.fr       */
+/*   Updated: 2022/10/20 20:59:57 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DEBUG_H
 # define DEBUG_H
+# define TUPLE_FORMAT TUPLE_FORMAT_0
+# define TUPLE_FORMAT_0 "(%3.3lf, %3.3lf, %3.3lf, %3.3lf)"
+# define TUPLE_FORMAT_1 "(%6.lf, %6.lf, %6.lf, %6.lf)"
+# define TUPLE_FORMAT_2 "(%12.12lf, %12.12lf, %12.12lf, %12.12lf)"
+# define MATRIX_FORMAT MATRIX_FORMAT_0
+# define MATRIX_FORMAT_0 "\
+|%3.3lf, %3.3lf, %3.3lf, %3.3lf|\n\
+|%3.3lf, %3.3lf, %3.3lf, %3.3lf|\n\
+|%3.3lf, %3.3lf, %3.3lf, %3.3lf|\n\
+|%3.3lf, %3.3lf, %3.3lf, %3.3lf|"
+# define MATRIX_FORMAT_1 "\
+|%6.lf, %6.lf, %6.lf, %6.lf|\n\
+|%6.lf, %6.lf, %6.lf, %6.lf|\n\
+|%6.lf, %6.lf, %6.lf, %6.lf|\n\
+|%6.lf, %6.lf, %6.lf, %6.lf|"
+# define MATRIX_FORMAT_2 "\
+|%12.12lf, %12.12lf, %12.12lf, %12.12lf|\n\
+|%12.12lf, %12.12lf, %12.12lf, %12.12lf|\n\
+|%12.12lf, %12.12lf, %12.12lf, %12.12lf|\n\
+|%12.12lf, %12.12lf, %12.12lf, %12.12lf|"
 
-typedef void	(*t_unravel)(t_object *, int);
+typedef struct s_unravel
+{
+	int		fd;
+	uint8_t	automatic : 1;
+	uint8_t	verbose : 1;
+	uint8_t	nests : 3;
+}	t_unravel;
+
+typedef void	(*t_unravels)(t_object *, t_unravel *);
 
 #endif
--- srcs/debug_print.c
+++ srcs/debug_print.c
@@ -6,40 +6,103 @@
 /*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 08:12:35 by thakala           #+#    #+#             */
-/*   Updated: 2022/10/19 10:04:52 by thakala          ###   ########.fr       */
+/*   Updated: 2022/10/20 21:38:47 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RTv1.h"
 
+static void	fd_retryer(int *fd, char template[])
+{
+	uint8_t	t;
+
+	t = 0;
+	while (*fd < 0)
+	{
+		if (++t > 100)
+		{
+			dprintf(STDERR_FILENO, \
+				"mkstemps failed 100 times in a row, quitting!\n");
+			break ;
+		}
+		printf("opening crash_situation fd: ");
+		*fd = mkstemps(template, 4);
+		printf("template: {'%s'}, fd: {'%d'}\n", template, *fd);
+	}
+}
+
+static void	get_fd_automatic(int *fd_automatic)
+{
+	char	template[] = "crash_situation_XXXXXXXXX.scn";
+
+	fd_retryer(fd_automatic, template);
+}
+
+static void	get_fd_user(int *fd_user)
+{
+	char		template[] = "user_XXXXXXXXX.scn";
+
+	fd_retryer(fd_user, template);
+}
+
+static void	get_fd(t_unravel *unravel)
+{
+	static int	fd_automatic = -1;
+
+	if (unravel->automatic)
+	{
+		if (fd_automatic < 0)
+		{
+			get_fd_automatic(&fd_automatic);
+			if (fd_automatic < 0)
+				return ;
+		}
+		unravel->fd = fd_automatic;
+	}
+	else
+		get_fd_user(&unravel->fd);
+}
+
 static char	*unravel_tuple(t_tuple *tuple)
 {
 	char	*res;
 
-	asprintf(&res, "\n");
+	asprintf(&res, TUPLE_FORMAT_0,
+		tuple->array[0],
+		tuple->array[1],
+		tuple->array[2],
+		tuple->array[3]);
+	return (res);
 }
 
-static char	*unravel_transform(
+static void	unravel_transform(
 	t_transform *transform,
-	char *translation_tuple,
-	char *rotation_tuple,
-	char *scale_tuple)
+	char **translation_tuple,
+	char **rotation_tuple,
+	char **scale_tuple)
 {
 	*translation_tuple = unravel_tuple(&transform->translation);
 	*rotation_tuple = unravel_tuple(&transform->rotation);
 	*scale_tuple = unravel_tuple(&transform->scale);
 }
 
-static char	*unravel_matrix_four(t_mtx_4 *matrix)
+static char	*unravel_matrix_four(t_mtx *m)
 {
 	char	*res;
 
-	asprintf(&res, "\n");
+	asprintf(&res, MATRIX_FORMAT_0,
+		m->array[0], m->array[1], m->array[2], m->array[3],
+		m->array[4], m->array[5], m->array[6], m->array[7],
+		m->array[8], m->array[9], m->array[10], m->array[11],
+		m->array[12], m->array[13], m->array[14], m->array[15]
+		);
+	return (res);
 }
 
-static void	unravel_plane(t_object *object, int fd)
+static void	unravel_plane(t_object *object, t_unravel *unravel)
 {
-	dprintf(fd, "\n");
+	(void)object;
+	dprintf(unravel->fd, "\n");
 }
 
 /*
@@ -47,7 +110,7 @@ Verbose mode in parallel to also print out the calculated matrices?
 Perhaps debug them elsewhere to keep this as a configuration file generator?
 */
 
-static void	unravel_sphere(t_object *object, int fd)
+static void	unravel_sphere(t_object *object, t_unravel *unravel)
 {
 	char	*origin_tuple;
 	char	*translation_tuple;
@@ -56,98 +119,109 @@ static void	unravel_sphere(t_object *object, int fd)
 	char	*colour_tuple;
 	char	*resultant_matrix;
 	char	*inverse_matrix;
+	int		status;
 
 	origin_tuple = unravel_tuple(&object->object.sphere.origin);
 	unravel_transform(
 		&object->object.sphere.transform,
-		translation_tuple,
-		rotation_tuple,
-		scale_tuple);
+		&translation_tuple,
+		&rotation_tuple,
+		&scale_tuple);
 	colour_tuple = unravel_tuple(&object->object.sphere.colour);
 	resultant_matrix = \
 		unravel_matrix_four(&object->object.sphere.transform.matrix.resultant);
 	inverse_matrix = \
 		unravel_matrix_four(&object->object.sphere.transform.matrix.inverse);
-	dprintf(fd, "\
-%s\
-%s\
-%s\
-%s\
-%s\
-%s\
-\n");
-}
-
-static void	unravel_cone(t_object *object, int fd)
-{
-	dprintf(fd, "\n");
-}
-
-static void	unravel_cylinder(t_object *object, int fd)
-{
-	dprintf(fd, "\n");
-}
-
-static void	unravel_camera(t_object *object, int fd)
-{
-	dprintf(fd, "\n");
-}
-
-static void	unravel_light(t_object *object, int fd)
-{
-	dprintf(fd, "\n");
-}
-
-static void	unravel_object_error(t_object *object, int fd)
-{
-	dprintf(fd, "\n");
-}
-
-static void	unravel_object_none(t_object *object, int fd)
-{
-	dprintf(fd, "\n");
-}
-
-static int	get_fd_automatic(void)
-{
-	static char	template[] = "crash_situation_XXXXXXXXX.scn";
-	int			fd_automatic;
-	uint8_t		t;
-
-	t = 0;
-	fd_automatic = -1;
-	while (fd_automatic < 0)
+	status = dprintf(unravel->fd, "\
+%s %s\n\
+%s %s\n\
+%s %s\n\
+%s %s\n\
+%s %s\n\
+%s %s\n\
+%s %s\n\
+\n",
+"origin", origin_tuple,
+"translation", translation_tuple,
+"rotation", rotation_tuple,
+"scale", scale_tuple,
+"colour", colour_tuple,
+"resultant", resultant_matrix,
+"inverse", inverse_matrix);
+	free(origin_tuple);
+	free(translation_tuple);
+	free(rotation_tuple);
+	free(scale_tuple);
+	free(colour_tuple);
+	free(resultant_matrix);
+	free(inverse_matrix);
+	if (status < 0)
 	{
-		if (++t > 100)
+		dprintf(STDERR_FILENO, "dprintf to fd %d failed!", unravel->fd);
+		close(unravel->fd);
+		unravel->fd = -1;
+		if (unravel->nests++ < 7)
 		{
-			printf("mkstemps failed 100 times in a row, quitting!\n");
-			break ;
+			get_fd(unravel);
+			unravel_sphere(object, unravel);
 		}
-		printf("opening crash_situation fd: ");
-		fd_automatic = mkstemps(template, 4);
-		printf("%d\n", fd_automatic);
 	}
-	return (fd_automatic);
 }
 
-int	unravel_objects_array(t_objects *objects, uint8_t automatic)
+static void	unravel_cone(t_object *object, t_unravel *unravel)
 {
-	static const t_unravel	\
-				unravel[] = {
+
+	(void)object;
+	dprintf(unravel->fd, "\n");
+}
+
+static void	unravel_cylinder(t_object *object, t_unravel *unravel)
+{
+
+	(void)object;
+	dprintf(unravel->fd, "\n");
+}
+
+static void	unravel_camera(t_object *object, t_unravel *unravel)
+{
+
+	(void)object;
+	dprintf(unravel->fd, "\n");
+}
+
+static void	unravel_light(t_object *object, t_unravel *unravel)
+{
+
+	(void)object;
+	dprintf(unravel->fd, "\n");
+}
+
+static void	unravel_object_error(t_object *object, t_unravel *unravel)
+{
+
+	(void)object;
+	dprintf(unravel->fd, "\n");
+}
+
+static void	unravel_object_none(t_object *object, t_unravel *unravel)
+{
+
+	(void)object;
+	dprintf(unravel->fd, "\n");
+}
+
+int	unravel_objects_array(t_objects *objects, t_unravel *unravel)
+{
+	static const t_unravels	\
+				unravels[] = {
 		unravel_plane, unravel_sphere,
 		unravel_cone, unravel_cylinder,
 		unravel_camera, unravel_light,
 		unravel_object_error, unravel_object_none
 	};
-	static int	fd_automatic = -1;
 	uint64_t	c;
 
-	if (automatic && fd_automatic < 0)
-	{
-		fd_automatic = get_fd_automatic();
-		if (fd_automatic < 0)
-			return (EXIT_FAILURE);
-	}
+	get_fd(unravel);
 	if (objects->len == 0)
 	{
 		dprintf(STDERR_FILENO, "No objects in objects array!\n");
@@ -156,7 +230,8 @@ int	unravel_objects_array(t_objects *objects, uint8_t automatic)
 	c = 0;
 	while (c < objects->len)
 	{
-		unravel[objects->list[c].type](&objects->list[c].object);
+		unravels[objects->list[c].type](&objects->list[c], unravel);
 		c++;
 	}
+	return (EXIT_SUCCESS);
 }
